0ebbb43e1930d67cb21d344548b3def1e2d1e4b8
Mehdi <mehdizaid02@gmail.com>
2024-02-12 01:17:12 +0100

    adding files

---
+++ main.py
@@ -0,0 +1,63 @@
+from dash import dash, dcc, html, Input, Output
+import pandas as pd
+
+from plots import boxplot_salary_gender_job_title, histogram_salary_company_size, \
+                histogram_weekly_hours_seniority_level, heatmap_median_salary
+from utils import get_frequent_company_locations
+
+
+app = dash.Dash(__name__)
+
+df_salaries = pd.read_csv("data/df_salaries.csv")
+
+app.layout = html.Div(children=[
+    html.H1(children='Which Job should I choose?', style={'margin': '0px', 'padding': '0'}),
+    html.Div([
+        html.Div([
+            dcc.Graph(
+                id='1',
+                figure=boxplot_salary_gender_job_title(df_salaries)
+            ),
+            dcc.Graph(
+                id='2',
+                figure=histogram_salary_company_size(df_salaries)
+            ),
+        ], style={'width': '49%', 'display': 'inline-block', 'margin': '0px', 'padding': '0'}),
+        html.Div([
+            dcc.Graph(
+                id='3',
+            ),
+            dcc.Dropdown(
+                id='country-dropdown',
+                options=[{'label': country, 'value': country} 
+                         for country in get_frequent_company_locations(df_salaries)],
+                value=['USA', 'GBR'],
+                multi=True,
+                style={'margin': '0px', 'padding': '0px'}
+            ),
+        ], style={'width': '49%', 'display': 'inline-block', 'margin': '0px', 'padding': '0'})
+    ]),
+    dcc.Graph(
+        id='4',
+        figure=heatmap_median_salary(df_salaries)
+    )
+], style={'margin': '0', 'padding': '0'})
+
+
+@app.callback(
+    Output('3', 'figure'),
+    [Input('country-dropdown', 'value')]
+)
+def update_histogram(selected_countries):
+    filtered_df = df_salaries[df_salaries['company_location'].isin(selected_countries)]
+    fig = histogram_weekly_hours_seniority_level(filtered_df)
+
+    return fig
+
+
+def main():
+    app.run_server(debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ plots.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import plotly.express as px
+
+
+def boxplot_salary_gender_job_title(df_salaries:pd.DataFrame):
+    fig = px.box(df_salaries, x='job_title_grouped', y='salary_in_usd', 
+                 color='gender', title='Boxplot of Salary by Gender and Job Type')
+    fig.update_layout(
+        xaxis=dict(title='Job Type', tickfont=dict(size=10)),
+        yaxis=dict(title='Salary (USD)', tickfont=dict(size=10)),
+        legend_title='Gender',
+        height=250,
+        showlegend=True,
+        margin=dict(t=50, b=50, l=50, r=50))
+    fig.update_xaxes(title_standoff=10)
+    fig.update_yaxes(title_standoff=10)
+
+    return fig
+
+
+def histogram_salary_company_size(df_salaries:pd.DataFrame):
+    fig = px.histogram(df_salaries, x='salary_in_usd', color='company_size', 
+                   histnorm='percent', barmode='overlay', title='Histogram of Salaries by Company Size')
+    fig.update_layout(
+        xaxis=dict(title='Salary (USD)', tickfont=dict(size=10)),
+        yaxis=dict(title='Percentage', tickfont=dict(size=10)),
+        legend_title='Company Size',
+        showlegend=True,
+        height=250,
+        margin=dict(t=50, b=50, l=50, r=50))
+    fig.update_xaxes(title_standoff=10)
+    fig.update_yaxes(title_standoff=10)
+
+    return fig
+
+
+def histogram_weekly_hours_seniority_level(df_salaries:pd.DataFrame):
+    fig = px.histogram(df_salaries, x='weekly_hours', color='company_location',
+                       title='Weekly Working Hours Distribution by Company Location',
+                       barmode='overlay', histnorm='percent', facet_row="remote_ratio")
+    
+    fig.update_layout(
+        xaxis=dict(title='Weekly Working Hours', tickfont=dict(size=10)),
+        yaxis=dict(title='Percentage', tickfont=dict(size=10)),
+        legend_title='Country',
+        showlegend=True,
+        margin=dict(t=50, b=50, l=50, r=50))
+    fig.update_xaxes(title_standoff=10)
+    fig.update_yaxes(title_text='Percentage', title_standoff=10)
+    fig.update_layout(height=500)
+    
+    return fig
+
+
+def heatmap_median_salary(df_salaries:pd.DataFrame):
+    median_salaries = df_salaries.groupby('company_location')['salary_in_usd'].median().reset_index()
+
+    fig = px.choropleth(median_salaries, 
+                    locations='company_location', 
+                    color='salary_in_usd', 
+                    color_continuous_scale=px.colors.sequential.Blues,
+                    range_color=(0, median_salaries['salary_in_usd'].max()),
+                    labels={'salary_in_usd': 'Median Salary (USD)'}
+                   )
+    fig.update_layout(
+        title_text="Median Salary by Country",
+        margin=dict(t=50, b=50, l=50, r=50),
+        title_y=0.5,
+        title_x=0.1,
+        height=530,
+        geo=dict(
+            showcoastlines=True)
+            )
+
+    return fig
+
+
+def main():
+    df_salaries = pd.read_csv("data/df_salaries.csv")
+    fig = heatmap_median_salary(df_salaries)
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
